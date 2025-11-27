3fb7d07febd5739c5584c66f4817e65010d60384
Alex Roup <alex@avraero.com>
2025-11-27 05:55:31 -0500

    implement multiple integration methods, add notes on future backsolving implementation requirements

--- include/control/Integrator.hpp
+++ include/control/Integrator.hpp
@@ -6,12 +6,17 @@
 #include <vector>
 
 namespace Control {
+typedef enum {
+    FwdEuler = 0,
+    BackEuler = 1,
+    Bilinear = 2
+} IntegMethod;
 
 class Integrator : public SISOBlock {
 
     public:
 
-        Integrator() : _dt(1.0f) {}
+        Integrator() : _dt(1.0f), _method(IntegMethod::FwdEuler) {}
 
         Limit limit;
         std::vector<Antiwindup> aw;
@@ -21,11 +26,12 @@ class Integrator : public SISOBlock {
         float step(float u);
         void setDt(float dt) { _dt = (dt>1e-6) ? dt : 1.0f; };
         float dt() const {return _dt;}
+        void setMethod(IntegMethod method) {_method = method;}
 
     protected:
 
         float _dt;
-
+        IntegMethod _method;
 
 };
 
--- src/control/Integrator.cpp
+++ src/control/Integrator.cpp
@@ -11,8 +11,44 @@ float Integrator::step(float in)
         awActive |= aw.at(k).isActive();
     }
 
+    // TODO: Backsolving
+    // For a backsolving implementation we only want to change the
+    // integrator state based on SISO saturation of the 
+    // integrator's output that occurs immediately downstream of the
+    // output of this class.  We do not want to feed back any
+    // filter dynamics or other static mappings.  Saturation information that
+    // cannot be returned exactly risks causing integrator windup --
+    // the very phenomenon that backsolving portends to mitigate.
+    // If the saturation cannot be returned exactly, then it is safer
+    // (and still reasonably performant) to use an antiwindup 
+    // detection mode and freeze the integrator rather than attempting 
+    // to correct it through backsolving.
+    // TODO: investigate a method that applies a small signal deadzone
+    // to suppress inexact backsolving while still performing backsolving
+    // on large errors.
+    // TODO: investigate dynamic inversion to approximately recover
+    // the unfiltered version of a signal for use in backsolving
+    // calculations.
+
     if (!awActive) {
-        _out = limit.step(_out + 0.5*(in + _in) * _dt);
+
+        switch (_method) {
+
+        case IntegMethod::BackEuler:
+            // Backward Euler
+            _out = limit.step(_out + in * _dt);
+            break;
+
+        case IntegMethod::FwdEuler:
+            // Forward Euler
+            _out = limit.step(_out + _in * _dt);
+            break;
+
+        case IntegMethod::Bilinear:
+            // Bilinear (Tustin)
+            _out = limit.step(_out + 0.5*(in + _in) * _dt);
+            break;
+        }
     } else {
         _out = limit.step(_out);
     }
