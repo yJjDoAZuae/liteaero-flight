253a0d65849fb134d1e79b039d2a31d1f5736ebd
Alex Roup <alex@avraero.com>
2025-11-27 08:06:48 -0500

    add Derivative for a numerical derivative block with specified rolloff denominator time constant

---
+++ include/control/Derivative.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "control/control.hpp"
+#include "control/SISOBlock.hpp"
+#include "control/Limit.hpp"
+#include "control/Antiwindup.hpp"
+#include <vector>
+
+namespace Control {
+
+class Derivative : public SISOBlock {
+
+    public:
+
+        Derivative() : _dt(1.0f), _Tau(0.0f), _method(DiscretizationMethod::FwdEuler) {}
+
+        Limit limit;
+
+        void reset(float u);
+
+        float step(float u);
+        void setDt(float dt) { _dt = (dt>1e-6) ? dt : 1.0f; };
+        void setTau(float Tau) { _Tau = Tau; };
+        float dt() const {return _dt;}
+        float Tau() const {return _Tau;}
+        void setMethod(DiscretizationMethod method) {_method = method;}
+
+    protected:
+
+        float _dt;
+        float _Tau;
+        DiscretizationMethod _method;
+
+};
+
+}
---
+++ src/control/Derivative.cpp
@@ -0,0 +1,30 @@
+#include "control/Derivative.hpp"
+
+using namespace Control;
+
+float Derivative::step(float in)
+{
+
+    _Tau = (_Tau > 4.0f*_dt) ? _Tau : 4.0f*_dt;
+
+    switch (_method) {
+
+    case DiscretizationMethod::BackEuler:
+        // Backward Euler
+        _out = limit.step(_Tau/(_Tau+_dt)*_out + (in - _in) / (_Tau + _dt));
+        break;
+
+    case DiscretizationMethod::FwdEuler:
+        // Forward Euler
+        _out = limit.step(_out * (_Tau - _dt)/_Tau + (in - _in) /_Tau);
+        break;
+
+    case DiscretizationMethod::Bilinear:
+        // Bilinear (Tustin)
+        _out = limit.step(_out * (2.0f*_Tau - _dt)/(2.0f*_Tau + _dt) + 0.5*(in + _in) * 2.0f/(2.0f*_Tau + _dt));
+        break;
+    }
+
+    _in = in;
+    return _out;
+}
