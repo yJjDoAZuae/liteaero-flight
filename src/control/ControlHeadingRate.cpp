0387a2f4304656d2a4f338fcc338fac96faab51e
Alex Roup <alex@avraero.com>
2025-12-21 08:24:14 -0500

    return horizontal plane acceleration command rather than roll angle

--- src/control/ControlHeadingRate.cpp
+++ src/control/ControlHeadingRate.cpp
@@ -1,15 +1,21 @@
 #include "control/ControlHeadingRate.hpp"
+#include <Eigen/Dense>
 
 using namespace Control;
+
+float velWind_horiz(const KinematicState & state)
+{
+    // velocity w.r.t. the wind in NED frame
+    Eigen::Vector3f velWind_NED_mps = state.q_nw().toRotationMatrix() * state.velocity_Wind_mps();
+
+    return hypot(velWind_NED_mps(0), velWind_NED_mps(1));
+}
 
 float ControlHeadingRate::step(float headingRateCmdIn, const KinematicState & state)
 {
     static constexpr float g = 9.81;
 
-    float TAS = state.velocity_Wind_mps().norm();
-    float ayCmd = TAS*headingRateCmdIn;
+    float ayCmd = velWind_horiz(state)*headingRateCmdIn;
 
-    float phiCmd = atan(ayCmd/g);
-
-    return phiCmd;
+    return ayCmd;
 }
