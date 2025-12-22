3a2dc1dab21d66c99e4bcd30801e4dfa300b9fd8
Alex Roup <alex@avraero.com>
2025-12-22 07:20:36 -0500

    load factor controller

---
+++ include/control/ControlLoadFactor.hpp
@@ -0,0 +1,16 @@
+
+#include "control/ControlLoop.hpp"
+#include "KinematicState.hpp"
+
+namespace Control {
+
+class ControlLoadFactor : public ControlLoop {
+
+    void configure();
+    void configure(json jsonConfig);
+    float step(float loadFactorCmdIn, const KinematicState & state);
+    void reset(float loadFactorCmdIn, const KinematicState & state);
+
+};
+
+}
---
+++ src/control/ControlLoadFactor.cpp
@@ -0,0 +1,17 @@
+#include "control/ControlLoadFactor.hpp"
+#include <Eigen/Dense>
+
+using namespace Control;
+
+static float constexpr g = 9.81;
+
+float ControlLoadFactor::step(float loadFactorCmdIn, const KinematicState & state)
+{
+    return pid.step(loadFactorCmdIn, -state.acceleration_Wind_mps()(2)/g);
+}
+
+void ControlLoadFactor::reset(float loadFactorCmdIn, const KinematicState & state)
+{
+    pid.reset(loadFactorCmdIn, -state.acceleration_Wind_mps()(2)/g, 0.0f);
+    pid.I.reset(0.0f);
+}
