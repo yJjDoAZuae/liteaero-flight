6ec11ffdeef4cf497338a8faf81aac599f7af42f
Alex Roup <alex@avraero.com>
2025-12-28 08:56:26 -0500

    roll control about the velocity vector rather than the Body x axis. TODO: verify eulerAngles(3,2,1) is correct here.

--- src/control/ControlRoll.cpp
+++ src/control/ControlRoll.cpp
@@ -5,11 +5,24 @@ using namespace Control;
 float ControlRoll::step(float cmdIn, const KinematicState & state)
 {
     pid.unwrapInputs = true;
-    return pid.step(cmdIn, state.roll(), state.rollRate_rps());
+
+    // get attitude Eulers of Wind frame
+    Eigen::Vector3f eulersWind = state.q_nw().toRotationMatrix().eulerAngles(3,2,1);
+
+    // get Body rates expressed in wind axes
+    Eigen::Vector3f ratesWind = state.q_nw().toRotationMatrix().transpose() * state.q_nb().toRotationMatrix() * state.rates_Body_rps();
+
+    // NOTE: roll and rollRate_rps aren't correct for velocity roll control
+    // use roll Euler of Wind frame w.r.t. NED and the roll rate of the Wind frame
+    return pid.step(cmdIn, eulersWind(0), state.rollRate_Wind_rps());
 }
 
 void ControlRoll::reset(float cmdIn, const KinematicState & state)
 {
     pid.unwrapInputs = true;
-    pid.reset(cmdIn, state.roll(), 0.0f);
+
+    // get attitude Eulers of Wind frame
+    Eigen::Vector3f eulersWind = state.q_nw().toRotationMatrix().eulerAngles(3,2,1);
+    
+    pid.reset(cmdIn, eulersWind(0), 0.0f);
 }
